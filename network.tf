5fc43c158a1348d833f484efc10cb4ee191f7cb9
shaikjaveedahmmed-dotcom <shaikjaveedahmmed@gmail.com>
2026-09-17 11:47:12 +0530

    Add subnet resource to network configuration

--- network.tf
+++ network.tf
@@ -5,3 +5,10 @@ resource "azurerm_virtual_network" "vnet" {
     location = "vijayanagaram"
   
 }
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
